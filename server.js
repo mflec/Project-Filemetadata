9e038a6d2a89a2c7f943f8be8b12b47ad17aebdc
Milagros Lechares <80794759+mflec@users.noreply.github.com>
2021-06-30 17:09:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const formidable = require("formidable");
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +12,21 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", (req, res, next) => {
+  const form = formidable({multiples: true});
+  form.parse(req, (err,fields,files) => {
+    if(err) {
+      next(err);
+      return;
+    }
 
-
+    res.json({
+      name: files.upfile.name,
+      type: files.upfile.type,
+      size: files.upfile.size
+    });
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
